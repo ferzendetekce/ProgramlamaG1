79a9e30627c18c92bf044418f26e234dd360049c
ferzendetekce <31697031+ferzendetekce@users.noreply.github.com>
2025-10-21 14:57:10 +0300

    Arka fon değişti

--- DevicesControllerApp/Kullanici/UserRegistration.Designer.cs
+++ DevicesControllerApp/Kullanici/UserRegistration.Designer.cs
@@ -67,6 +67,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
