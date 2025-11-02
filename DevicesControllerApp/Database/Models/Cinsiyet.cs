5c006a4b0034a427dc63d0eed4e76ff8c9376412
Kaan Emre Uğurlu <kaanemreugurlu@gmail.com>
2025-11-02 16:29:59 +0300

    Sql dosyası ve database modelleri eklendi

---
+++ DevicesControllerApp/Database/Models/Cinsiyet.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace LokomatUygulamasi.Modeller
+{
+    [Table("cinsiyetler")]
+    public class Cinsiyet
+    {
+        [Key]
+        [Column("cinsiyet_id")]
+        public int CinsiyetId { get; set; }
+        [Required]
+        [Column("cinsiyet_adi")]
+        public string CinsiyetAdi { get; set; }
+    }
+}
---
+++ DevicesControllerApp/Database/Models/Log.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace LokomatUygulamasi.Modeller
+{
+    [Table("loglar")]
+    public class Log
+    {
+        [Key]
+        [Column("log_id")]
+        public long LogId { get; set; }
+        [Required]
+        [Column("log_seviyesi")]
+        public string LogSeviyesi { get; set; }
+        [Required]
+        [Column("mesaj")]
+        public string Mesaj { get; set; }
+        [Column("olay_zamani")]
+        public DateTime? OlayZamani { get; set; }
+        [Column("hesap_id")]
+        public int? HesapId { get; set; }
+        [Column("ip_adresi")]
+        public string IpAdresi { get; set; }
+    }
+}
