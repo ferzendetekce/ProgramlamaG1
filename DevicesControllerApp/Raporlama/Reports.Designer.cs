deae7ed498045adba286574f08ac542518eb9ed3
ahsencansuduzgun <ahsen.duzgun@ogr.dpu.edu.tr>
2025-11-02 15:08:35 +0300

    .

--- DevicesControllerApp/Raporlama/Reports.Designer.cs
+++ DevicesControllerApp/Raporlama/Reports.Designer.cs
@@ -29,31 +29,44 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button1.Location = new System.Drawing.Point(510, 261);
+            this.button1.Location = new System.Drawing.Point(680, 321);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(338, 142);
+            this.button1.Size = new System.Drawing.Size(451, 175);
             this.button1.TabIndex = 0;
             this.button1.Text = "Rapor Al";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(491, 151);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
             // Reports
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Reports";
-            this.Size = new System.Drawing.Size(1343, 705);
+            this.Size = new System.Drawing.Size(1791, 868);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
