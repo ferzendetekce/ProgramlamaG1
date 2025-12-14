78f0ab73fc1f85ba337aaac9a12cf767d398ccd1
Yahya Sagdic <yahyasgdc@gmail.com>
2025-12-14 16:22:58 +0300

    v2.1

--- DevicesControllerApp/Ana ekran-Login/MainForm.Designer.cs
+++ DevicesControllerApp/Ana ekran-Login/MainForm.Designer.cs
@@ -54,10 +54,9 @@
             // statusStrip1
             // 
             this.statusStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
-            this.statusStrip1.Location = new System.Drawing.Point(0, 1132);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 578);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Padding = new System.Windows.Forms.Padding(2, 0, 28, 0);
-            this.statusStrip1.Size = new System.Drawing.Size(2140, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(1070, 22);
             this.statusStrip1.TabIndex = 0;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -65,7 +64,6 @@
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Margin = new System.Windows.Forms.Padding(6);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -87,27 +85,25 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
-            this.splitContainer1.Size = new System.Drawing.Size(2140, 1132);
-            this.splitContainer1.SplitterDistance = 328;
-            this.splitContainer1.SplitterWidth = 8;
+            this.splitContainer1.Size = new System.Drawing.Size(1070, 578);
+            this.splitContainer1.SplitterDistance = 164;
             this.splitContainer1.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(24, 1027);
-            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Location = new System.Drawing.Point(12, 534);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(266, 77);
+            this.button1.Size = new System.Drawing.Size(133, 40);
             this.button1.TabIndex = 11;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button11
             // 
-            this.button11.Location = new System.Drawing.Point(24, 938);
-            this.button11.Margin = new System.Windows.Forms.Padding(6);
+            this.button11.Location = new System.Drawing.Point(12, 488);
             this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(266, 77);
+            this.button11.Size = new System.Drawing.Size(133, 40);
             this.button11.TabIndex = 10;
             this.button11.Text = "button11";
             this.button11.UseVisualStyleBackColor = true;
@@ -115,10 +111,9 @@
             // 
             // button10
             // 
-            this.button10.Location = new System.Drawing.Point(24, 850);
-            this.button10.Margin = new System.Windows.Forms.Padding(6);
+            this.button10.Location = new System.Drawing.Point(12, 442);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(266, 77);
+            this.button10.Size = new System.Drawing.Size(133, 40);
             this.button10.TabIndex = 9;
             this.button10.Text = "Portlara Bağlanma";
             this.button10.UseVisualStyleBackColor = true;
@@ -126,10 +121,9 @@
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(24, 762);
-            this.button9.Margin = new System.Windows.Forms.Padding(6);
+            this.button9.Location = new System.Drawing.Point(12, 396);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(266, 77);
+            this.button9.Size = new System.Drawing.Size(133, 40);
             this.button9.TabIndex = 8;
             this.button9.Text = "button9";
             this.button9.UseVisualStyleBackColor = true;
@@ -137,20 +131,19 @@
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(24, 673);
-            this.button8.Margin = new System.Windows.Forms.Padding(6);
+            this.button8.Location = new System.Drawing.Point(12, 350);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(266, 77);
+            this.button8.Size = new System.Drawing.Size(133, 40);
             this.button8.TabIndex = 7;
             this.button8.Text = "button8";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // btnSettings
             // 
-            this.btnSettings.Location = new System.Drawing.Point(24, 585);
-            this.btnSettings.Margin = new System.Windows.Forms.Padding(6);
+            this.btnSettings.Location = new System.Drawing.Point(12, 304);
             this.btnSettings.Name = "btnSettings";
-            this.btnSettings.Size = new System.Drawing.Size(266, 77);
+            this.btnSettings.Size = new System.Drawing.Size(133, 40);
             this.btnSettings.TabIndex = 6;
             this.btnSettings.Text = "AYARLAR";
             this.btnSettings.UseVisualStyleBackColor = true;
@@ -158,10 +151,9 @@
             // 
             // btnService
             // 
-            this.btnService.Location = new System.Drawing.Point(24, 496);
-            this.btnService.Margin = new System.Windows.Forms.Padding(6);
+            this.btnService.Location = new System.Drawing.Point(12, 258);
             this.btnService.Name = "btnService";
-            this.btnService.Size = new System.Drawing.Size(266, 77);
+            this.btnService.Size = new System.Drawing.Size(133, 40);
             this.btnService.TabIndex = 5;
             this.btnService.Text = "SERVİS";
             this.btnService.UseVisualStyleBackColor = true;
@@ -169,10 +161,9 @@
             // 
             // btnReports
             // 
-            this.btnReports.Location = new System.Drawing.Point(24, 408);
-            this.btnReports.Margin = new System.Windows.Forms.Padding(6);
+            this.btnReports.Location = new System.Drawing.Point(12, 212);
             this.btnReports.Name = "btnReports";
-            this.btnReports.Size = new System.Drawing.Size(266, 77);
+            this.btnReports.Size = new System.Drawing.Size(133, 40);
             this.btnReports.TabIndex = 4;
             this.btnReports.Text = "RAPORLAMA";
             this.btnReports.UseVisualStyleBackColor = true;
@@ -180,10 +171,9 @@
             // 
             // btnMonitoring
             // 
-            this.btnMonitoring.Location = new System.Drawing.Point(24, 319);
-            this.btnMonitoring.Margin = new System.Windows.Forms.Padding(6);
+            this.btnMonitoring.Location = new System.Drawing.Point(12, 166);
             this.btnMonitoring.Name = "btnMonitoring";
-            this.btnMonitoring.Size = new System.Drawing.Size(266, 77);
+            this.btnMonitoring.Size = new System.Drawing.Size(133, 40);
             this.btnMonitoring.TabIndex = 3;
             this.btnMonitoring.Text = "REHABİLİTASYON İZLEME";
             this.btnMonitoring.UseVisualStyleBackColor = true;
@@ -191,10 +181,9 @@
             // 
             // btnUsers
             // 
-            this.btnUsers.Location = new System.Drawing.Point(24, 231);
-            this.btnUsers.Margin = new System.Windows.Forms.Padding(6);
+            this.btnUsers.Location = new System.Drawing.Point(12, 120);
             this.btnUsers.Name = "btnUsers";
-            this.btnUsers.Size = new System.Drawing.Size(266, 77);
+            this.btnUsers.Size = new System.Drawing.Size(133, 40);
             this.btnUsers.TabIndex = 2;
             this.btnUsers.Text = "KULLANICI KAYIT";
             this.btnUsers.UseVisualStyleBackColor = true;
@@ -202,10 +191,9 @@
             // 
             // btnPatient
             // 
-            this.btnPatient.Location = new System.Drawing.Point(24, 142);
-            this.btnPatient.Margin = new System.Windows.Forms.Padding(6);
+            this.btnPatient.Location = new System.Drawing.Point(12, 74);
             this.btnPatient.Name = "btnPatient";
-            this.btnPatient.Size = new System.Drawing.Size(266, 77);
+            this.btnPatient.Size = new System.Drawing.Size(133, 40);
             this.btnPatient.TabIndex = 1;
             this.btnPatient.Text = "HASTA KAYIT";
             this.btnPatient.UseVisualStyleBackColor = true;
@@ -213,10 +201,9 @@
             // 
             // btnTherapy
             // 
-            this.btnTherapy.Location = new System.Drawing.Point(24, 54);
-            this.btnTherapy.Margin = new System.Windows.Forms.Padding(6);
+            this.btnTherapy.Location = new System.Drawing.Point(12, 28);
             this.btnTherapy.Name = "btnTherapy";
-            this.btnTherapy.Size = new System.Drawing.Size(266, 77);
+            this.btnTherapy.Size = new System.Drawing.Size(133, 40);
             this.btnTherapy.TabIndex = 0;
             this.btnTherapy.Text = "TERAPİ";
             this.btnTherapy.UseVisualStyleBackColor = true;
@@ -226,7 +213,6 @@
             // 
             this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer2.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer2.Margin = new System.Windows.Forms.Padding(6);
             this.splitContainer2.Name = "splitContainer2";
             this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
@@ -237,19 +223,17 @@
             // splitContainer2.Panel2
             // 
             this.splitContainer2.Panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.splitContainer2.Size = new System.Drawing.Size(1804, 1132);
-            this.splitContainer2.SplitterDistance = 133;
-            this.splitContainer2.SplitterWidth = 8;
+            this.splitContainer2.Size = new System.Drawing.Size(902, 578);
+            this.splitContainer2.SplitterDistance = 67;
             this.splitContainer2.TabIndex = 0;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2140, 1154);
+            this.ClientSize = new System.Drawing.Size(1070, 600);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.statusStrip1);
-            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "MainForm";
             this.Text = "Devices Controller Main";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
--- DevicesControllerApp/Cihaz Haberlesme Protokolleri/DeviceCommunication.cs
+++ DevicesControllerApp/Cihaz Haberlesme Protokolleri/DeviceCommunication.cs
@@ -10,6 +10,12 @@ namespace RehabilitationSystem.Communication
     {
         private static DeviceCommunication _instance;
         private static readonly object _lock = new object();
+        private Dictionary<byte, ManualResetEvent> _responseWaiters = new Dictionary<byte, ManualResetEvent>();
+        private Dictionary<byte, byte[]> _responseData = new Dictionary<byte, byte[]>();
+        private readonly object _responseLock = new object();
+        private float _simulatedSpeed = 50.0f;
+        private Dictionary<int, int> _simulatedMotorPositions = new Dictionary<int, int>();
+
 
         private SerialPort _serialPort;
         private Thread _readThread;
@@ -35,6 +41,8 @@ namespace RehabilitationSystem.Communication
         public string CurrentPort { get; private set; }
         public int BaudRate { get; private set; }
         public int CommandTimeout { get; set; } = 1000; // ms
+
+        public bool SimulationMode { get; set; } = false; // Test için
 
         // Singleton Instance
         public static DeviceCommunication Instance
@@ -212,21 +220,31 @@ namespace RehabilitationSystem.Communication
 
         private void DispatchReceivedPacket(byte commandCode, byte[] payload)
         {
-            // Komut koduna göre işlem yap (Enum: CommandCode)
+            // Önce yanıt bekleyen var mı kontrol et
+            lock (_responseLock)
+            {
+                if (_responseWaiters.ContainsKey(commandCode))
+                {
+                    _responseData[commandCode] = payload;
+                    _responseWaiters[commandCode].Set(); // Bekleyeni uyandır
+                    return; // Event fırlatma, sadece bekleyene ver
+                }
+            }
+
+            // Yanıt bekleyen yoksa normal işlem
             CommandCode code = (CommandCode)commandCode;
 
             switch (code)
             {
-                case CommandCode.ReadLoadCell: // Örnek: LoadCell verisi geldi
+                case CommandCode.ReadLoadCell:
                     ParseLoadCellData(payload);
                     break;
 
-                case CommandCode.ReadStatus: // Cihaz durumu geldi
-                                             // ParseDeviceStatus(payload); // Bu metodu sonra yazarız
+                case CommandCode.ReadStatus:
+                    // ParseDeviceStatus(payload);
                     break;
 
                 default:
-                    // Genel komut yanıtı olarak event fırlat
                     OnCommandResponseReceived(commandCode, payload);
                     break;
             }
@@ -254,10 +272,138 @@ namespace RehabilitationSystem.Communication
             }
         }
 
-        public byte[] SendCommandAndWaitResponse(byte commandCode, byte[] data = null,
-            int timeoutMs = 0)
+        public byte[] SendCommandAndWaitResponse(byte commandCode, byte[] data = null, int timeoutMs = 0)
         {
-            return null;
+            if (timeoutMs == 0)
+                timeoutMs = CommandTimeout;
+
+            try
+            {
+                //  SİMÜLASYON MODU: Gerçek cihaz olmadan test
+                if (SimulationMode)
+                {
+                    LogCommunication($"[SİMÜLASYON] Komut 0x{commandCode:X2} gönderildi, sahte yanıt üretiliyor...");
+                    Thread.Sleep(100); // Gerçekçi gecikme
+                    return GenerateSimulatedResponse(commandCode, data);
+                }
+
+                // Normal mod (gerçek cihazla)
+                ManualResetEvent waitHandle = new ManualResetEvent(false);
+
+                lock (_responseLock)
+                {
+                    _responseWaiters[commandCode] = waitHandle;
+                    if (_responseData.ContainsKey(commandCode))
+                        _responseData.Remove(commandCode);
+                }
+
+                if (!SendCommand(commandCode, data))
+                {
+                    lock (_responseLock)
+                    {
+                        _responseWaiters.Remove(commandCode);
+                    }
+                    return null;
+                }
+
+                bool received = waitHandle.WaitOne(timeoutMs);
+
+                lock (_responseLock)
+                {
+                    _responseWaiters.Remove(commandCode);
+
+                    if (received && _responseData.ContainsKey(commandCode))
+                    {
+                        byte[] response = _responseData[commandCode];
+                        _responseData.Remove(commandCode);
+                        return response;
+                    }
+                }
+
+                LogCommunication($"TIMEOUT! Komut 0x{commandCode:X2} için {timeoutMs}ms içinde yanıt gelmedi.", true);
+                return null;
+            }
+            catch (Exception ex)
+            {
+                LogCommunication($"SendCommandAndWaitResponse hatası: {ex.Message}", true);
+                return null;
+            }
+        }
+
+        private byte[] GenerateSimulatedResponse(byte commandCode, byte[] sentData)
+        {
+            CommandCode cmd = (CommandCode)commandCode;
+
+            switch (cmd)
+            {
+                case CommandCode.Connect:
+                    return new byte[] { 0x01 };
+
+                case CommandCode.Disconnect:
+                    return new byte[] { 0x01 };
+
+                case CommandCode.SetSpeed:
+                    // Gönderilen hızı kaydet
+                    if (sentData != null && sentData.Length >= 4)
+                    {
+                        _simulatedSpeed = BitConverter.ToSingle(sentData, 0);
+                    }
+                    return new byte[] { 0x01 };
+
+                case CommandCode.GetSpeed:
+                    // Kaydedilmiş hızı döndür
+                    return BitConverter.GetBytes(_simulatedSpeed);
+
+                case CommandCode.SetStepMotor:
+                    // Motor hareketini simüle et
+                    if (sentData != null && sentData.Length >= 5)
+                    {
+                        int motorIndex = sentData[0];
+                        int steps = BitConverter.ToInt32(sentData, 1);
+
+                        if (!_simulatedMotorPositions.ContainsKey(motorIndex))
+                            _simulatedMotorPositions[motorIndex] = 0;
+
+                        _simulatedMotorPositions[motorIndex] += steps;
+
+                        LogCommunication($"[SİMÜLASYON] Motor {motorIndex}: {_simulatedMotorPositions[motorIndex] - steps} → {_simulatedMotorPositions[motorIndex]}");
+                    }
+                    return new byte[] { 0x01 };
+
+                case CommandCode.GetMotorPosition:
+                    // Motor pozisyonunu döndür
+                    int requestedMotor = 0;
+
+                    if (sentData != null && sentData.Length >= 1)
+                    {
+                        requestedMotor = sentData[0];
+                    }
+
+                    if (!_simulatedMotorPositions.ContainsKey(requestedMotor))
+                        _simulatedMotorPositions[requestedMotor] = 0;
+
+                    return BitConverter.GetBytes(_simulatedMotorPositions[requestedMotor]);
+
+                case CommandCode.StartTherapy:
+                    return new byte[] { 0x01 };
+
+                case CommandCode.StopTherapy:
+                    return new byte[] { 0x01 };
+
+                case CommandCode.PauseTherapy:
+                    return new byte[] { 0x01 };
+
+                case CommandCode.ResumeTherapy:
+                    return new byte[] { 0x01 };
+
+                case CommandCode.EmergencyStop:
+                    LogCommunication("[SİMÜLASYON] ACİL DURDURMA aktif!");
+                    return new byte[] { 0x01 };
+
+                default:
+                    LogCommunication($"[SİMÜLASYON] Bilinmeyen komut: 0x{commandCode:X2}");
+                    return new byte[] { 0x00 };
+            }
         }
 
         private byte[] BuildCommandPacket(byte commandCode, byte[] data)
@@ -537,6 +683,80 @@ namespace RehabilitationSystem.Communication
             return false;
         }
 
+        public bool Connect()
+        {
+            LogCommunication("Cihaza bağlanılıyor...");
+            byte[] response = SendCommandAndWaitResponse((byte)CommandCode.Connect, null, 2000);
+
+            if (response != null)
+            {
+                LogCommunication("✓ Cihaz bağlantısı onaylandı!");
+                return true;
+            }
+            else
+            {
+                LogCommunication("✗ Cihaz bağlantısı başarısız (timeout)!", true);
+                return false;
+            }
+        }
+
+        public bool Disconnect()
+        {
+            LogCommunication("Cihaz bağlantısı kesiliyor...");
+            byte[] response = SendCommandAndWaitResponse((byte)CommandCode.Disconnect, null, 2000);
+
+            if (response != null)
+            {
+                LogCommunication("✓ Cihaz bağlantısı kesildi!");
+                return true;
+            }
+            else
+            {
+                LogCommunication("✗ Disconnect timeout!", true);
+                return false;
+            }
+        }
+
+        public bool SetSpeedWithConfirmation(double speed)
+        {
+            LogCommunication($"Hız ayarlanıyor: {speed}");
+
+            byte[] speedData = BitConverter.GetBytes((float)speed);
+            byte[] response = SendCommandAndWaitResponse((byte)CommandCode.SetSpeed, speedData, 1500);
+
+            if (response != null)
+            {
+                LogCommunication($"✓ Hız başarıyla {speed} olarak ayarlandı!");
+                return true;
+            }
+            else
+            {
+                LogCommunication("✗ Hız ayarlama timeout!", true);
+                return false;
+            }
+        }
+
+        public double GetCurrentSpeed()
+        {
+            LogCommunication("Mevcut hız sorgulanıyor...");
+
+            byte[] response = SendCommandAndWaitResponse((byte)CommandCode.GetSpeed, null, 1000);
+
+            if (response != null && response.Length >= 4)
+            {
+                float speed = BitConverter.ToSingle(response, 0);
+                LogCommunication($"✓ Mevcut hız: {speed}");
+                return speed;
+            }
+            else
+            {
+                LogCommunication("✗ Hız okuma başarısız (timeout veya geçersiz veri)!", true);
+                return -1;
+            }
+        }
+
+
+
         private void ParseLoadCellData(byte[] payload)
         {
             // Beklenen veri boyutu kontrolü (4 float + 1 int = 20 byte)
@@ -667,7 +887,48 @@ namespace RehabilitationSystem.Communication
             });
         }
 
-        // Mevcut "return false" dönen metotları bunlarla değiştirin:
+        public bool MoveMotor(int motorIndex, int steps)
+        {
+            LogCommunication($"Motor {motorIndex} -> {steps} adım ilerletiliyor...");
+
+            List<byte> payload = new List<byte>();
+            payload.Add((byte)motorIndex);
+            payload.AddRange(BitConverter.GetBytes(steps));
+
+            byte[] response = SendCommandAndWaitResponse((byte)CommandCode.SetStepMotor, payload.ToArray(), 2000);
+
+            if (response != null)
+            {
+                LogCommunication($"✓ Motor {motorIndex} başarıyla {steps} adım ilerledi!");
+                return true;
+            }
+            else
+            {
+                LogCommunication($"✗ Motor hareket timeout!", true);
+                return false;
+            }
+        }
+
+        public int GetMotorPosition(int motorIndex)
+        {
+            LogCommunication($"Motor {motorIndex} pozisyonu sorgulanıyor...");
+
+            byte[] motorIndexData = new byte[] { (byte)motorIndex };
+            byte[] response = SendCommandAndWaitResponse((byte)CommandCode.GetMotorPosition, motorIndexData, 1000);
+
+            if (response != null && response.Length >= 4)
+            {
+                int position = BitConverter.ToInt32(response, 0);
+                LogCommunication($"✓ Motor {motorIndex} pozisyonu: {position}");
+                return position;
+            }
+            else
+            {
+                LogCommunication($"✗ Motor pozisyon okuma başarısız!", true);
+                return -1;
+            }
+        }
+
 
         public bool StartTherapy()
         {
@@ -1019,7 +1280,9 @@ namespace RehabilitationSystem.Communication
         SetServoMotor = 0x40,
         SetStepMotor = 0x41,
         LoadPattern = 0x50,
-        HomeDevice = 0x51
+        HomeDevice = 0x51,
+        GetSpeed = 0x25, // YENİ
+        GetMotorPosition = 0x42, // YENİ
     }
 
     #endregion
